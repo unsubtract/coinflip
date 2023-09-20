340089296c3aa0390b82f081ac89e8dd0a99e915
unsubtract <52540509+unsubtract@users.noreply.github.com>
2023-09-20 09:12:22 -0400

    cleanup build script

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-CC="gcc -pipe"
-CFLAGS="-Wall -Wextra -Wpedantic -s -Os -fsanitize=address"
+CC="gcc"
+CFLAGS="-pipe -Wall -Wextra -Wpedantic -std=c99 -s -Os -flto"
 # largely based on https://github.com/Francesco149/nolibc-httpd
-CFLAGS_NOLIBC="-DNOLIBC -Wall -Wextra -Wpedantic -s -Os -flto \
-    -nostdlib -ffreestanding -no-pie -fno-stack-protector \
-    -fdata-sections -ffunction-sections -fno-unwind-tables -fno-asynchronous-unwind-tables \
-	-Wl,-n -Wl,--gc-sections -Wl,--build-id=none"
+CFLAGS_NOLIBC="${CFLAGS} -DNOLIBC -nostdlib -ffreestanding -no-pie
+    -fno-stack-protector -fdata-sections -ffunction-sections -fno-unwind-tables
+    -fno-asynchronous-unwind-tables -Wl,-n -Wl,--gc-sections -Wl,--build-id=none"
 
 [ "$NOLIBC" ] && $CC $CFLAGS_NOLIBC coin.c crt.S -o coin || $CC $CFLAGS coin.c -o coin
-strip -R .comment ./coin
+strip -R .comment coin
