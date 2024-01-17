c7d37e2116c73b0203124a07fe2a18d9e90eebc4
unsubtract <saihaj.sran@gmail.com>
2024-01-17 17:34:37 -0500

    remove _start symbol, entry point is main instead

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 CC="gcc"
 CFLAGS="-pipe -Wall -Wextra -Wpedantic -std=c99 -s -Os -flto"
 # largely based on https://github.com/Francesco149/nolibc-httpd
-CFLAGS_NOLIBC="-DNOLIBC -nostdlib -ffreestanding -no-pie
+CFLAGS_NOLIBC="-DNOLIBC -nostdlib -ffreestanding -no-pie --entry=main
     -fno-stack-protector -fdata-sections -ffunction-sections -fno-unwind-tables
     -fno-asynchronous-unwind-tables -Wl,-n -Wl,--gc-sections -Wl,--build-id=none"
 
